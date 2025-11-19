65337cf708940c3d7a09cc27e9641bad27736540
Alvin Tiong <alvintiong086330664@gmail.com>
2025-11-19 10:57:42 +0800

    membership_process.php

--- membership_process.php
+++ membership_process.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Root Flower">
+    <meta name="keywords" content="Flowers, Shop, Kuching, Sarawak, Malaysia">
+    <meta name="author" content="Daniel, Josiah, Alvin, Kheldy">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="Pictures/Index/logo.png" type="image/png">
+    <title>Root Flower</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+
 <?php
 
 // Step 1: Connect to the database
