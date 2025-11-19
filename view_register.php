7793ad5f3b5ddd56bd06e79835e766def1c1807f
daniel123 <danielwilliem@gmail.com>
2025-11-19 10:18:29 +0800

    Seperate admin sidebar

--- view_register.php
+++ view_register.php
@@ -30,16 +30,8 @@ $result = $conn->query($sql);
 <body>
 
 <main class="admin-main">
-	<!-- Sidebar (matches admin_panel.php) -->
-	<div class="admin-sidebar">
-		<h2>Admin Panel</h2>
-		<a href="view_register.php">Workshop Registrations</a>
-		<a href="view_enquiry.php">Enquiries</a>
-		<a href="view_membership.php">Memberships</a>
-		<a href="view_login.php">Logins</a>
-		<a href="recyclebin.php">Recycle Bin</a>
-		<a href="index.php">Go back to Home</a>
-	</div>
+	<!-- Sidebar -->
+    <?php include 'admin_sidebar.php'; ?>
 
 	<!-- Content area: table lives here -->
 	<div class="admin-content">
